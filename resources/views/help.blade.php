5e285d2e9a14b96f3aaa473568ae631b2b340cbb
afistrayyan3131 <afist.urfa@mhs.unsoed.ac.id>
2023-06-15 22:45:36 +0700

    mengedit tahapan peminjaman 2

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -12,12 +12,12 @@
       <!-- End Hero -->
       <!-- Start How To Use PERSAFRAT -->
       <div class="how-use-simanuk my-4 container">
-          <h2>TAHAPAN PEMINJAMAN RUANGAN</h2>
+          <h2>TAHAPAN PEMINJAMAN ALAT - RUANGAN</h2>
           <div class="list-tahapan p-5">
               <ul>
                   <li>Untuk melakukan peminjaman, Anda diharuskan melakukan login terlebih dahulu sesuai dengan username yang telah disediakan oleh admin pada saat sosialisasi PESRAFAT.</li>
-                  <li>Jika ingin mengetahui alat/ruangan yang tersedia, silakan menuju ke menu Daftar Ruangan. Secara default, tampilan akan langsung ke menu Daftar Ruangan.</li>
-                  <li>Jika ingin meminjam alat/ruangan, silakan menuju ke menu Daftar Ruangan.</li>
+                  <li>Jika ingin mengetahui alat/ruangan yang tersedia, silakan menuju ke menu Daftar Alat Ruangan. Secara default, tampilan akan langsung ke menu Daftar Alat Ruangan.</li>
+                  <li>Jika ingin meminjam alat/ruangan, silakan menuju ke menu Daftar Alat Ruangan.</li>
                   <li>Pastikan data yang Anda masukkan sudah sesuai. Jika sudah sesuai, klik Tambah.</li>
                   <li>Proses pengajuan peminjaman sedang diproses, silakan tunggu pemberitahuan lebih lanjut.</li>
                   <li>Untuk mengecek status peminjaman apakah diterima atau ditolak, silakan menuju sub-menu Daftar Peminjaman.</li>
