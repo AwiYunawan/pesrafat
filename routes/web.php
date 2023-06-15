4080642135edfb2f4b75228f109e1fdc78648000
AwiYunawan <hisagikurokiba@gmail.com>
2023-06-15 20:17:46 +0700

    menambahkan routing untuk login, admin, dan user

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardRoomController;
+use App\Http\Controllers\DashboardUserController;
+use App\Http\Controllers\DashboardAdminController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +22,17 @@ Route::get('/', function () {
 });
 
 Route::resource('dashboard/rooms', DashboardRoomController::class)->middleware('auth');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('dashboard/users/{id}/makeAdmin', [DashboardUserController::class, 'makeAdmin'])->middleware('auth');
+
+Route::resource('dashboard/users', DashboardUserController::class)->middleware('auth');
+
+Route::get('dashboard/admin/{id}/removeAdmin', [DashboardAdminController::class, 'removeAdmin'])->middleware('auth');
+
+Route::resource('dashboard/admin', DashboardAdminController::class)->middleware('auth');
