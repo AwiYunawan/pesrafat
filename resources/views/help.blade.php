37e50144ba3e2f9deb80f3c46e513cb674a13cec
afistrayyan3131 <afist.urfa@mhs.unsoed.ac.id>
2023-06-15 22:42:45 +0700

    mengedit petunjuk penggunaan

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -17,8 +17,8 @@
               <ul>
                   <li>Untuk melakukan peminjaman, Anda diharuskan melakukan login terlebih dahulu sesuai dengan username yang telah disediakan oleh admin pada saat sosialisasi PESRAFAT.</li>
                   <li>Jika ingin mengetahui alat/ruangan yang tersedia, silakan menuju ke menu Daftar Ruangan. Secara default, tampilan akan langsung ke menu Daftar Ruangan.</li>
-                  <li>Jika ingin meminjam alat/ruangan, silakan menuju ke menu Daftar Ruangan. Cek ketersediaan alat/ruangan dan daftar peminjam pada sub-menu Daftar Ruangan dan Daftar Peminjam. Jika ruangan tersedia, silakan klik tombol Pinjam dan isi form peminjaman. </li>
-                  <li>Pastikan data yang Anda masukkan sudah sesuai. Jika sudah sesuai, klik Kirim.</li>
+                  <li>Jika ingin meminjam alat/ruangan, silakan menuju ke menu Daftar Ruangan.</li>
+                  <li>Pastikan data yang Anda masukkan sudah sesuai. Jika sudah sesuai, klik Tambah.</li>
                   <li>Proses pengajuan peminjaman sedang diproses, silakan tunggu pemberitahuan lebih lanjut.</li>
                   <li>Untuk mengecek status peminjaman apakah diterima atau ditolak, silakan menuju sub-menu Daftar Peminjaman.</li>
               </ul>
