7b9fa09df5fb91076dc1b3251627c28f94f7252e
syifarahmadina <radinasyifa7@gmail.com>
2023-06-15 21:34:03 +0700

    membuat tampilan layout dan navbar footer sidebar

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class=" col-md-2 col-6 p-0 sidebar">
+    <ul class="nav flex-column ">
+      
+      <li class="nav-item">
+        <a class="nav-link" href="/dashboard/admin">Daftar Petugas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/dashboard/users">Daftar Peminjam</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/dashboard/rooms">Daftar Alat Ruangan</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/dashboard/rents">Daftar Peminjaman</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/dashboard/temporaryRents">Konfirmasi Peminjaman</a>
+      </li>
+    </ul>
+</div>
